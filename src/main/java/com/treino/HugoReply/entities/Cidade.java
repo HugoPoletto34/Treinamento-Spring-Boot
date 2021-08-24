c43cb1d66c07612441a7deba3576aef65dd3a5bb
h.poletto <h.poletto@reply.com>
2021-08-24 10:05:27 -0300

    Classe Cidade e Enum Unidade Federativa

---
+++ src/main/java/com/treino/HugoReply/entities/Cidade.java
@@ -0,0 +1,27 @@
+package com.treino.HugoReply.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_cidade")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Cidade implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private UnidadeFederativa uf;
+
+}
